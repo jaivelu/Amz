de371ef7fb4be089b53c51caf9ddeb64a44f90f0
jaivelu <jayamvelu65@gmail.com>
2022-10-29 19:23:10 +0530

    First Message

---
+++ src/test/java/org/test/TestGit.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class TestGit {
+
+
+	private void one() {
+		System.out.println("1");	
+	}
+
+
+	private void two() {
+		System.out.println("2");
+	}
+	
+	private void three() {
+
+		System.out.println("3");
+	}
+
+}
